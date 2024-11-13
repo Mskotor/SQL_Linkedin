438ccc2b025c1cb42c95012c7be150e48ddf60d7
mskotor <mskotor@interia.pl>
2024-11-13 23:02:26 +0000

    Section 4

--- app.py
+++ app.py
@@ -1,9 +1,70 @@
 import sqlite3
 
-
 # << Connect to the database >>
-database = sqlite3.connect('WSDA_Music.db')
+connection = sqlite3.connect('WSDA_Music.db')
 
 # << Create a cursor object >>
-cursor = database.cursor()
+cursor = connection.cursor()
 
+# << Basic Query >>
+# result = connection.execute("""
+# SELECT
+#     FirstName,
+#     LastName,
+#     Email
+# FROM
+#     Customer
+# """)
+
+# << Column alias >>
+# result = connection.execute("""
+# SELECT
+#     FirstName AS Name,
+#     LastName AS Surname
+#     Email
+# FROM
+#     Customer
+# """)
+
+# << Sorting >>
+# Default = ASC, DESC (descending)
+# result = connection.execute("""
+# SELECT
+#     FirstName AS Name,
+#     LastName AS Surname,
+#     Email
+# FROM
+#     Customer
+# ORDER BY
+#     Surname DESC
+# """)
+
+# << Limiting results >>
+# result = connection.execute("""
+# SELECT
+#     FirstName AS Name,
+#     LastName AS Surname,
+#     Email
+# FROM
+#     CUstomer
+# ORDER BY
+#     Surname DESC,
+#     Name
+# LIMIT
+#     10
+# """)
+
+# << Section 4 exercise >>
+result = connection.execute(""""
+SELECT
+    name
+FROM
+    Track
+WHERE
+    type = 'table' AND name = 'Name'
+""").fetchall()
+
+
+
+# for row in result:
+#     print(row)
